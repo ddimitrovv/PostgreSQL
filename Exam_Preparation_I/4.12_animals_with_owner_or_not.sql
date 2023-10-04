3d1cbcc0ee64352c4fbdc00976ef0f485bca6aa6
ddimitrovv <dimitrov.mitko.md@gmail.com>
2023-10-04 10:06:48 +0300

    added tasks from Exam Preparation I

--- Exam_Preparation_I/4.12_animals_with_owner_or_not.sql
+++ Exam_Preparation_I/4.12_animals_with_owner_or_not.sql
@@ -19,6 +19,27 @@ BEGIN
 END;
 $$;
 
+
+-- Second solution 
+CREATE OR REPLACE PROCEDURE sp_animals_with_owners_or_not (
+	IN animal_name VARCHAR(30),
+	OUT owner_name VARCHAR(50)
+)
+LANGUAGE plpgsql 
+AS $$  
+BEGIN
+    owner_name := (SELECT owners.name
+    FROM animals
+    LEFT JOIN owners ON animals.owner_id = owners.id
+    WHERE animals.name = animal_name);
+
+    IF owner_name IS NULL THEN
+        owner_name := 'For adoption';
+    END IF;
+    RAISE INFO '%', owner_name;
+END;
+$$;
+
 -- To use the stored procedure, you can call it as follows:
 CALL sp_animals_with_owners_or_not('Pumpkinseed Sunfish');
 CALL sp_animals_with_owners_or_not('Hippo');
