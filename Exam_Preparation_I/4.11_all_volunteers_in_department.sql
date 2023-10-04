25d002f0901e7230a42e378c81125cbf29dd50c3
ddimitrovv <dimitrov.mitko.md@gmail.com>
2023-10-04 09:47:56 +0300

    added tasks from Exam Preparation I

--- Exam_Preparation_I/4.11_all_volunteers_in_department.sql
+++ Exam_Preparation_I/4.11_all_volunteers_in_department.sql
@@ -20,3 +20,23 @@ LANGUAGE plpgsql;
 SELECT fn_get_volunteers_count_from_department('Education program assistant');
 SELECT fn_get_volunteers_count_from_department('Guest engagement');
 SELECT fn_get_volunteers_count_from_department('Zoo events');
+
+-- Second solution
+CREATE OR REPLACE FUNCTION fn_get_volunteers_count_from_department (
+    searched_volunteers_department VARCHAR(30)
+)
+RETURNS INTEGER AS $$
+DECLARE 
+    count_of_volunteers INTEGER;
+BEGIN
+	count_of_volunteers := (
+		SELECT count(*)
+		FROM volunteers_departments vd
+		JOIN volunteers v ON vd.id = v.department_id
+		WHERE vd.department_name = searched_volunteers_department
+	);
+
+    RETURN count_of_volunteers;
+END;
+$$
+LANGUAGE plpgsql;
